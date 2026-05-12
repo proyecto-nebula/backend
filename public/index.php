fbee1c8df99052db862c69d0a18c2d687b6be0a1
ivanguiort <ivan.guiort@gmail.com>
2026-05-12 10:59:51 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use App\core\router;
+use App\Core\Router;
 
 $router = new Router();
 
@@ -15,4 +15,4 @@ header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { exit; } // Responder a preflight requests
 // Ejecutar Router
-\App\core\router::dispatch(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+\App\Core\Router::dispatch(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
