d12c276e702cce03ed4119e863670bfb1e9444f2
ivanguiort <ivan.guiort@gmail.com>
2026-05-12 11:05:22 +0200

    Refactor index.php to improve router handling

--- public/index.php
+++ public/index.php
@@ -1,18 +1,21 @@
 <?php
 
-require_once __DIR__ . '/../bootstrap.php';
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../bootstrap.php';
 
 use App\Core\Router;
 
-$router = new Router();
-
 // CORS
 $frontendUrl = $_ENV['FRONTEND_URL'] ?? 'http://localhost:4200';
+
 header("Access-Control-Allow-Origin: $frontendUrl");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { exit; } // Responder a preflight requests
-// Ejecutar Router
-\App\Core\Router::dispatch(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    exit;
+}
+
+// Router (SIN INSTANCIAR)
+Router::dispatch($_SERVER['REQUEST_URI']);
